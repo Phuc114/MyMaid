9b2c2a5f85c5fabae9fea124e0b22fa3eb9013d1
Soizz <gphucdx@gmail.com>
2025-07-17 20:51:15 +0700

    Initial commit

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Homepage.css';
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Homepage;
